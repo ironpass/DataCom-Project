2ba97c0195a5fe66f196bc8eae5a79e81c2a5593
Ood <akarachai45525@hotmail.com>
2017-11-23 16:11:19 +0700

    fixed

--- src/Executer.java
+++ src/Executer.java
@@ -1,12 +1,12 @@
 import java.io.IOException;
 
 public class Executer implements Runnable {
-	private String cmd = "cmd.exe /c cd pythonsrc && start " ;
+	private String cmd = "cmd.exe /c cd pythonsrc" ;
 
 	public Executer(String subnetMask, String defaultGateway, String startIp,
 			String limit, String retry, String maxthread, String filename) {
 		// Command for execution
-		cmd += "python main.py " + subnetMask + " " + defaultGateway + " "
+		cmd += " && start python main.py " + subnetMask + " " + defaultGateway + " "
 				+ startIp + " " + limit + " " + retry + " " + maxthread + " "
 				+ filename;
 	}
@@ -15,7 +15,7 @@ public class Executer implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 		try {
-			System.out.println("Executing...");
+			System.out.println("Executing: " + cmd);
 			Process p = Runtime.getRuntime().exec(cmd);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
